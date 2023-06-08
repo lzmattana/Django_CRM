7ef5c30250d1bba2a86a498c8c1496ae4ef3af5e
Leonardo Mattana <l.z.mattana@gmail.com>
2023-06-07 22:18:23 -0300

    login user

--- website_CRM/views.py
+++ website_CRM/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if login in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate the user
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Logged in successfully')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('home')
+    else:       
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    pass
